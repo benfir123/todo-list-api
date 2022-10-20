db0ffb6c969e951f039c3c994bfc78e728e92b32
Ben Chanapai <ben_chanapai04@hotmail.com>
2022-10-20 12:29:58 +0700

    Add comments

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 
+# register the ViewSet route - this creates urls and path names based on
+# predefined function names such as create, list, and destroy
 router.register("todos", views.TodoViewSet, basename="todo")
 
 urlpatterns = [
